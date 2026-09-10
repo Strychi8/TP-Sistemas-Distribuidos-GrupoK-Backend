ad783921c8d031d59bb6c9474c0c089a5798a525
Strychi8 <tomas2000T@gmail.com>
2026-09-10 10:55:30 -0300

    config: configuracion de Swagger inicial

---
+++ web_services/src/main/java/com/empresa_rentar/web_services/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.empresa_rentar.web_services.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    OpenAPI empresaRentarOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Empresa Rentar - API REST - Grupo K")
+                        .description("API REST para la gestión de vehículos, clientes y reservas de alquileres.")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Repositorio del proyecto en GitHub")
+                        .url("https://github.com/Strychi8/TP-Sistemas-Distribuidos-GrupoK-Backend"));
+    }
+}
