22af50c88d6f9d5d4c7d21188e654973d43ee7e2
Strychi8 <tomas2000T@gmail.com>
2026-09-09 21:32:23 -0300

    feat: inicializar proyecto Spring Boot base

---
+++ web_services/src/main/java/com/empresa_rentar/web_services/WebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.empresa_rentar.web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesApplication.class, args);
+	}
+
+}
