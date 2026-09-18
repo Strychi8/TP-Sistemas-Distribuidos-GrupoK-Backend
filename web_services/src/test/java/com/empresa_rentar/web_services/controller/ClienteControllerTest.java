cb581f1db303da26ebddac19cf39aa9acf6b0948
Strychi8 <tomas2000T@gmail.com>
2026-09-18 11:25:33 -0300

    test: actualizar dependencias y anotaciones para compatibilidad con Spring Boot 4

--- web_services/src/test/java/com/empresa_rentar/web_services/controller/ClienteControllerTest.java
+++ web_services/src/test/java/com/empresa_rentar/web_services/controller/ClienteControllerTest.java
@@ -11,8 +11,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
@@ -54,7 +54,7 @@ class ClienteControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private IClienteService clienteService;
 
     @Test
