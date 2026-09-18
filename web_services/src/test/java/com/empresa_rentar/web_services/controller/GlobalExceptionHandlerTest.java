2a67ca95a51bf757de33f768898b28c72a2eca71
Tomas Strycharski <tomas2000T@gmail.com>
2026-09-18 11:30:54 -0300

    test: corregir configuración de pruebas para Spring Boot 4 (#11)

--- web_services/src/test/java/com/empresa_rentar/web_services/controller/GlobalExceptionHandlerTest.java
+++ web_services/src/test/java/com/empresa_rentar/web_services/controller/GlobalExceptionHandlerTest.java
@@ -5,8 +5,8 @@ import com.empresa_rentar.web_services.service.IClienteService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -42,7 +42,7 @@ class GlobalExceptionHandlerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private IClienteService clienteService;
 
     @Test
