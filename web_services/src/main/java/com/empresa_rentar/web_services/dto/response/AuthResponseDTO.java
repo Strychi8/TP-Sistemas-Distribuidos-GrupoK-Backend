a79bbad2ba728fb3642162390d9d7400d9b2a9de
PiniolEnzo <165811482+PiniolEnzo@users.noreply.github.com>
2026-09-21 19:53:24 -0300

    feat(auth): Retornar email y roles en la respuesta de login

--- web_services/src/main/java/com/empresa_rentar/web_services/dto/response/AuthResponseDTO.java
+++ web_services/src/main/java/com/empresa_rentar/web_services/dto/response/AuthResponseDTO.java
@@ -5,10 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponseDTO {
     private String token;
+    private String email;
+    private List<String> roles;
 }
